9966bf9b9b5f4e50386f58a0acd1147d7a528e0c
Johannes Erchen <johannes@erchen-it.de>
2020-12-04 10:37:57 +0100

    DAY-01: Solve Day 01 Challenge Part 2

--- src/main/java/net/erchen/adventofcode/day01/Day01.java
+++ src/main/java/net/erchen/adventofcode/day01/Day01.java
@@ -1,15 +1,36 @@
 package net.erchen.adventofcode.day01;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Day01 {
 
-    public static int solvePuzzle(Set<Integer> input) {
-        return input.stream()
-                .filter(a -> input.contains(2020 - a))
+    public static int part1(Set<Integer> input) {
+        return multipleTogether(findSummandsFor2020(input).orElseThrow());
+    }
+
+    public static int part2(Set<Integer> input) {
+        return multipleTogether(input.stream()
+                .flatMap(a -> findSummandsFor2020(input, a).stream())
                 .findFirst()
-                .map(a -> a * (2020 - a))
-                .orElse(0);
+                .orElseThrow());
+    }
+
+    private static Optional<List<Integer>> findSummandsFor2020(Set<Integer> input, int... additionalSummands) {
+        var targetSum = 2020 - IntStream.of(additionalSummands).sum();
+
+        return input.stream()
+                .filter(i -> input.contains(targetSum - i))
+                .findFirst()
+                .map(i -> IntStream.concat(IntStream.of(i, targetSum - i), IntStream.of(additionalSummands)).boxed().collect(toList()));
+    }
+
+    private static int multipleTogether(List<Integer> summands) {
+        return summands.stream().reduce((a, b) -> a * b).orElseThrow();
     }
 
 }
--- src/test/java/net/erchen/adventofcode/day01/Day01Test.java
+++ src/test/java/net/erchen/adventofcode/day01/Day01Test.java
@@ -12,28 +12,53 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Day01Test {
 
-    @Test
-    void demoInput() {
-        // given
-        var input = Set.of(1721, 979, 366, 299, 675, 1456);
+    static final Set<Integer> DEMO_INPUT = Set.of(1721, 979, 366, 299, 675, 1456);
 
+    static Set<Integer> solutionInput() throws IOException {
+        return Files.readAllLines(Path.of("src/test/resources/day01/input.txt")).stream().map(Integer::valueOf).collect(toSet());
+    }
+
+    @Test
+    void part1DemoInput() {
         // when
-        var solution = Day01.solvePuzzle(input);
+        var solution = Day01.part1(DEMO_INPUT);
 
         // then
         assertThat(solution).isEqualTo(514579);
     }
 
     @Test
-    void solutionInput() throws IOException {
+    void part1SolutionInput() throws IOException {
         // given
-        var input = Files.readAllLines(Path.of("src/test/resources/day01/input.txt")).stream().map(Integer::valueOf).collect(toSet());
+        var input = solutionInput();
 
         // when
-        var solution = Day01.solvePuzzle(input);
+        var solution = Day01.part1(input);
 
         // then
         assertThat(solution).isEqualTo(703131);
+    }
+
+    @Test
+    void part2DemoInput() {
+        // when
+        var solution = Day01.part2(DEMO_INPUT);
+
+        // then
+        assertThat(solution).isEqualTo(241861950);
+    }
+
+
+    @Test
+    void part2SolutionInput() throws IOException {
+        // given
+        var input = solutionInput();
+
+        // when
+        var solution = Day01.part2(input);
+
+        // then
+        assertThat(solution).isEqualTo(272423970);
     }
 
 }
